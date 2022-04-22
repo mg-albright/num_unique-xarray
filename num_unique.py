6a0552e2786a6a7b6c580cefd924b890a5b4d521
Mary Grace Albright <89799653+mg-albright@users.noreply.github.com>
2022-04-22 14:35:18 -0400

    Create as function
    Adapted from script to a function

--- num_unique.py
+++ num_unique.py
@@ -14,42 +14,49 @@ import dask.dataframe as dd
 # import dataset
 ds = xr.open_dataset('path_to_dataset')
 
-# Will need to extract target variable if there are multiple in dataset
+# Will need to extract target variable as xarray if there are multiple in dataset
 
-# I don't want to count the zeros, so, here, I'm setting 0 to NaN
-ds = ds.where(ds > 0)
+# create function
+def num_unique(ds):
+    
+    # I don't want to count the zeros, so, here, I'm setting 0 to NaN
+    ds = ds.where(ds > 0)
 
-# get size of lat, lon, and time
-lat = len(ds.lat)
-lon = len(ds.lon)
-time = len(ds.time)
+    # get size of lat, lon, and time
+    n_lat = len(ds.lat)
+    n_lon = len(ds.lon)
+    n_time = len(ds.time)
+    
+    # get lat and lon arrays
+    lat = ds.lat
+    lon = ds.lon
 
-# convert to array and reshape
-ds_array = ds.to_array()
-ds_array =  ds_array.values.reshape(time,lat*lon) # values denotes target variable
+    # convert to array and reshape
+    ds_array = ds.to_array()
+    ds_array =  ds_array.values.reshape(n_time,n_lat*n_lon) # values denotes target variable
 
-# convert to dask DataFrame and compute
-ds_df = dd.from_array(ds_array).compute()
+    # convert to dask DataFrame and compute
+    ds_df = dd.from_array(ds_array).compute()
 
-# calculate number of unique instances
-num_unique = ds_df.nunique()
+    # calculate number of unique instances
+    count_num_unique = ds_df.nunique()
 
-# convert to numpy and reshape back to lat/lon grid
-np_ds = num_unique.to_numpy()
-new_ds = np_ds.reshape(lat,lon)
+    # convert to numpy and reshape back to lat/lon grid
+    np_ds = count_num_unique.to_numpy()
+    new_ds = np_ds.reshape(n_lat,n_lon)
 
-# recreate xarray, if needed
-unique_xr = xr.Dataset(
-    data_vars=dict(
-        variable=(["lat","lon"], new_ds)
-    ),
-    coords=dict(
-        lat=(["lat"], lat),
-        lon=(["lon"], lon)
-    ),
-)
-
-
+    # recreate xarray, if needed
+    unique_xr = xr.Dataset(
+        data_vars=dict(
+            variable=(["lat","lon"], new_ds)
+        ),
+        coords=dict(
+            lat=(["lat"], lat),
+            lon=(["lon"], lon)
+        ),
+    )
+    
+    return unique_xr
 
 
 
